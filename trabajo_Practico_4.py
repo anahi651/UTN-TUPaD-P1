3fb06730bf9a92ce16a5da17d968252a48ae1e3d
anahi651 <anahi.betancort@gmail.com>
2025-04-13 15:49:23 -0300

    se sube ejercicio 4 y 5

--- trabajo_Practico_4.py
+++ trabajo_Practico_4.py
@@ -32,3 +32,34 @@ if numero > numero_dos:
 for x in range (numero+1 , numero_dos):
     suma +=x #suma=suma +x
 print("La suma de los números entre", numero, "y", numero_dos, "es:", suma)
+
+#4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
+#secuencia. El programa debe detenerse y mostrar el total acumulado cuando el usuario ingrese
+#un 0.
+CORTE=0    #constante corte como valor 0
+
+numero= int(input("Ingrese un número entero: "))
+suma=0 #inicializa la suma 
+while numero != CORTE: #si es distinto a 0 entra en el bucle 
+    suma+=numero #= suma cada número ingresado 
+    numero= int(input("Ingrese otro número entero:"))
+    
+print("El total acumulado es de:", suma)#si se ingreso 0 sale del bucle y muestra el acumulado
+
+
+print("//////////////////////////////////////////")
+
+#5) Crea un juego en el que el usuario deba adivinar un número aleatorio entre 0 y 9. Al final, el
+#programa debe mostrar cuántos intentos fueron necesarios para acertar el número.
+
+from random import *#Importa todas las funciones del módulo 'random' permite generar numeros aleatorios 
+numero_aleatorio=randint(0, 9)
+suma=0 #inicializa suma para contar intentos 
+numero_ingresado= int(input("Adivine el número, es entre el 0 y el 9: "))#Pide al usuario que ingrese un número y lo convierte en entero. Este es el primer intento
+
+while numero_ingresado!= numero_aleatorio:   # Mientras el número ingresado sea distinto del número aleatorio, repite el bucle
+    suma=suma+1                                # Suma 1 a la variable 'suma' por cada intento incorrecto
+    numero_ingresado = int(input("Adivine de nuevo: "))     # Pide al usuario otro número en cada vuelta del bucle
+
+print("¡Correcto!")#sale del bucle si adivino 
+print("La cantidad de intentos necesarios para adivinar el número fue:", suma + 1)#suma 1 para incluir el primer intento
