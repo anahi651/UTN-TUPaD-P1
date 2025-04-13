64b177b3ab5b0be10b9d3d8efb55256e55d09830
anahi651 <anahi.betancort@gmail.com>
2025-04-13 14:07:47 -0300

    Se sube ejercicio 1 y 2

---
+++ trabajo_Practico_4.py
@@ -0,0 +1,22 @@
+#1.Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100 (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+for x in range(101): # comenzando desde 0 por defecto, y se incrementa de 1 (por defecto), y finaliza en un número indicado sin incluir .
+    print(x)
+
+#2. Desarrolla un programa que solicite al usuario un número entero y determine la cantidad de dígitos que contiene.
+
+numero=int(input("Ingrese un número entero: "))#Se solicita un núemero entero al usuario
+
+
+
+# Si el número es 0, tiene 1 dígito
+if numero == 0:
+    cantidad_digitos = 1
+else:
+    cantidad_digitos = 0
+    while numero > 0:
+        numero = numero // 10  # División entera, elimina el último dígito
+        cantidad_digitos += 1  # Contamos ese dígito
+
+print("El número tiene", cantidad_digitos, "dígitos.")
+  
